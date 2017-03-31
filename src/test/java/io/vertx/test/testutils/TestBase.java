da32412fa294250d119b9dec18858e785c2f3a4c
Mihai Stanescu <mihai.stanescu@qnective.com>
2017-03-31 10:27:45 +0200

    Added TestBase helper class

---
+++ src/test/java/io/vertx/test/testutils/TestBase.java
@@ -0,0 +1,38 @@
+package io.vertx.test.testutils;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import org.junit.After;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+
+import static io.vertx.test.testutils.AwaitUtils.awaitResult;
+
+
+public class TestBase
+{
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    private CompletableFuture<AssertionError> assertions = new CompletableFuture<>();
+
+    protected Vertx vertx;
+
+    protected void startVertx(VertxOptions vertxOptions) throws Exception
+    {
+        vertx = awaitResult(
+                h -> Vertx.clusteredVertx(
+                        vertxOptions,
+                        h
+                )
+        );
+
+        vertx.exceptionHandler(
+                event ->
+                {
+                    assertions.complete((AssertionError) event);
+                }
+        );
+    }
+}
