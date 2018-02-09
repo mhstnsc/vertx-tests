0c271d12fe0a036d2e15bc281d22752b3802c965
Mihai Stanescu <mihai.stanescu@qnective.com>
2018-02-09 20:26:21 +0100

    New hazelcast utils

---
+++ src/test/java/io/mhstnsc/vertx/test/testutils/HazelcastUtils.java
@@ -0,0 +1,23 @@
+package io.mhstnsc.vertx.test.testutils;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import io.vertx.core.Vertx;
+import io.vertx.core.impl.VertxImpl;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+import java.util.Set;
+
+
+public class HazelcastUtils
+{
+
+    public static HazelcastInstance getHazelcastInstance(Vertx vertx)
+    {
+        VertxImpl vimpl = (VertxImpl) vertx;
+        HazelcastClusterManager clusterManager = (HazelcastClusterManager) vimpl.getClusterManager();
+        return clusterManager.getHazelcastInstance();
+    }
+
+}
