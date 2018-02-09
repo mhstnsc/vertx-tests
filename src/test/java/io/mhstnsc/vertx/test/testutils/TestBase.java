8f323ee64615ab66d9537f491981b3c55f0dd958
Mihai Stanescu <mihai.stanescu@qnective.com>
2018-02-09 20:26:48 +0100

    Change TestBase to deploy clustered or not clustered vertx

--- src/test/java/io/mhstnsc/vertx/test/testutils/TestBase.java
+++ src/test/java/io/mhstnsc/vertx/test/testutils/TestBase.java
@@ -2,6 +2,9 @@ package io.mhstnsc.vertx.test.testutils;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+import org.junit.After;
+import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,20 +15,43 @@ import static io.mhstnsc.vertx.test.AwaitUtils.awaitResult;
 
 public class TestBase
 {
-    protected Logger logger = LoggerFactory.getLogger(getClass());
-
     private CompletableFuture<AssertionError> assertions = new CompletableFuture<>();
+
+    private boolean useClusteredVertx;
 
     protected Vertx vertx;
 
-    protected void startVertx(VertxOptions vertxOptions) throws Exception
+    public TestBase()
     {
-        vertx = awaitResult(
-                h -> Vertx.clusteredVertx(
-                        vertxOptions,
-                        h
-                )
-        );
+        this.useClusteredVertx = false;
+    }
+
+    protected TestBase(boolean useClusteredVertx)
+    {
+        this.useClusteredVertx = useClusteredVertx;
+    }
+
+    @Before
+    public void setup() throws Exception
+    {
+        if(useClusteredVertx)
+        {
+            VertxOptions vertxOptions = new VertxOptions()
+                    .setClustered(true)
+                    .setClusterManager(new HazelcastClusterManager());
+
+            vertx = awaitResult(
+                    h -> Vertx.clusteredVertx(
+                            vertxOptions,
+                            h
+                    )
+            );
+        }
+        else
+        {
+            vertx = Vertx.vertx();
+        }
+
 
         vertx.exceptionHandler(
                 event ->
@@ -33,5 +59,11 @@ public class TestBase
                     assertions.complete((AssertionError) event);
                 }
         );
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+        vertx.close();
     }
 }
